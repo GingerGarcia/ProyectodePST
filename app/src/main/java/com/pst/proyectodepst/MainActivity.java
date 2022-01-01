17289b364eff6eb3e022897969bf525bf2a79c8a
Cesarq19 <cepaquin@espol.edu.ec>
2022-01-01 16:53:12 -0500

    Colocacion de componentes y parte logica del MainActivity

--- app/src/main/java/com/pst/proyectodepst/MainActivity.java
+++ app/src/main/java/com/pst/proyectodepst/MainActivity.java
@@ -2,7 +2,9 @@ package com.pst.proyectodepst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // Metodo del botón Start
+    public void start(View view){
+        Intent siguiente = new Intent(this,Activity_2.class);
+        startActivity(siguiente);
     }
 }
