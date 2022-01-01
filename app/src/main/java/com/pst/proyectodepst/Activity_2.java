17a19a6f59519516c47f716aed5e0685922cc961
Cesarq19 <cepaquin@espol.edu.ec>
2022-01-01 18:37:16 -0500

    Inicio de los metodos log in y sign up en la Activity2

--- app/src/main/java/com/pst/proyectodepst/Activity_2.java
+++ app/src/main/java/com/pst/proyectodepst/Activity_2.java
@@ -3,12 +3,26 @@ package com.pst.proyectodepst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Activity_2 extends AppCompatActivity {
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+    // Metodo del boton log in
+    public void login(View view){
+        username = (EditText) findViewById(R.id.editTextUsername);
+        password = (EditText) findViewById(R.id.editTextPassword);
+
+    }
+    // Metodo del boton sign up
+    public void signup(View view){
+        username = (EditText) findViewById(R.id.editTextUsername);
+        password = (EditText) findViewById(R.id.editTextPassword);
     }
 }
