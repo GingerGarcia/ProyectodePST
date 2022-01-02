0d1a73519ada02b3d4b703120d71a430a9028eb1
PauloToro <ptoro@espol.edu.ec>
2022-01-02 14:53:50 -0500

    forntend de la ventana iniciar sesion, la pantalla principal de la app y agregados los recursos de backgorund personalizado con sus logos

---
+++ app/src/main/java/com/pst/proyectodepst/insesion.java
@@ -0,0 +1,14 @@
+package com.pst.proyectodepst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insesion);
+    }
+}
