64ca4f27d63e1668b06a6ce85b872ea0bd6a0954
PauloToro <ptoro@espol.edu.ec>
2022-01-02 18:58:33 -0500

    Creada a la ventana de accion para el escanner, habilitando la camara, ademas de habilitar permisos de la camera para escanear un QR.

--- app/src/main/java/com/pst/proyectodepst/Activity_4.java
+++ app/src/main/java/com/pst/proyectodepst/Activity_4.java
@@ -3,12 +3,38 @@ package com.pst.proyectodepst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class Activity_4 extends AppCompatActivity {
+import com.google.zxing.Result;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Activity_4 extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView escanerView;
+    private String nombreCaja;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+    }
+
+    public void EscanerQR(View view){
+        escanerView=new ZXingScannerView(this);
+        setContentView(escanerView);
+        escanerView.setResultHandler(this);
+        escanerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        escanerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        nombreCaja=result.getText();
+        escanerView.resumeCameraPreview(this);
+
     }
 }
